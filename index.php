4b3d75f7382f09fb024e57a7ce4ae3f900412cfc
idwsdta <clemwm@gmail.com>
2015-01-07 16:22:59 +0200

    initv0
    init

---
+++ index.php
@@ -0,0 +1,175 @@
+<?php 
+    require_once 'header.php';
+    require_once 'nav.php'; 
+?>
+       
+        <div class="row">
+            <div class="homeskybg background" data-img-width="2560" data-img-height="505">
+                <div class="container" style="padding-bottom: 20px;">
+                    <div class="col-xs-7">
+                    <div class="row idms-home-data-header-text" >
+                            <div class="header-main-title-1024">
+                                <span style="padding-left:20px">TURN DATA</span> <br>
+                                 <span style="padding-left:20px"> INTO INFORMATION</span>
+                            </div>
+                            <div class="header-main-title-768">
+                                <span style="padding-left:20px">TURN DATA INTO INFORMATION</span>
+                            </div>
+                                
+                        </div>
+                        <div class="row idms-home-data-header-body" >
+                           <h3 style="padding-left:20px"> Welcome Mr Name,</h3>
+                             <p style="padding-left:20px">
+                                venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. 
+                                Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. 
+                                Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, 
+                                dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. 
+                                Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. 
+                                Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero,
+                                sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. 
+                                Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. 
+                                Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla 
+                                mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales,
+                                augue velit cursus nunc,  
+                             </p>
+                             <span style="padding-left:20px ; font-size:14px">Regard,</span><br>
+                             <span style="padding-left:20px ;font-size:14px">Name, post</span>
+                        </div>
+                    </div> 
+                    <div class="col-xs-5">Right</div>
+
+                </div>
+            </div> 
+        </div>
+        <div class="row homemostviewed">
+            <div class="container">
+                <div class="row">
+                    <h3 style="padding-left:25px ; font-size:30px; color:#001965" >MOST VIEWED WIDGETS</h3>
+                </div>
+                <div class="row">
+                    <div id="idms-widget-carousel" class="carousel slide " >       
+                        <center>
+                        <ul class="carousel-inner" style="width: 80%;">   
+                            <li class="item active" class="" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+                                <div class="row">
+                                    <div class="col-xs-6 item-content">
+                                        left
+                                    </div>
+                                    <div class="col-xs-6 item-content">
+                                       right 
+                                    </div>
+                                </div>
+
+                            </li>
+                            <li class="item">
+                                <div class="row">
+                                    <div class="col-xs-6 item-content ">
+                                       left 
+                                    </div>
+                                    <div class="col-xs-6 item-content ">
+                                      right  
+                                    </div>
+                                </div>
+
+                            </li> 
+                            <li class="item">
+                                <div class="row">
+                                    <div class="col-xs-6 item-content">
+                                        left
+                                    </div>
+                                    <div class="col-xs-6 item-content">
+                                      right  
+                                    </div>
+                                </div>
+
+                            </li>
+                        </ul>
+                            </center>
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li id="home-image-slide-modal-control-0" data-target="#idms-widget-carousel" data-slide-to="0" class="active"></li>
+                            <li id="home-image-slide-modal-control-1" data-target="#idms-widget-carousel" data-slide-to="1" class="active"></li>
+                            <li id="home-image-slide-modal-control-2"data-target="#idms-widget-carousel" data-slide-to="2"></li>
+                        </ol>
+                        <!--Image slide Popped-->
+                        <div id="home-image-slide-modal-0" class="idms-slide-popover">
+                            <div class="row">
+                                    <div class="col-xs-6 item-content-popover">
+                                        over
+                                    </div>
+                                </div> 
+                        </div>
+                        <div id="home-image-slide-modal-1" class="idms-slide-popover  hide">
+                             <div class="row">
+                                    <div class="col-xs-6 item-content-popover">
+                                        over1
+                                    </div>
+                                </div>
+                        </div>
+                        <div id="home-image-slide-modal-2" class="idms-slide-popover hide">
+                            <div class="row">
+                                    <div class="col-xs-6 item-content-popover">
+                                        over2
+                                    </div>
+                                </div>
+                        </div>
+                        <!-- Controls -->
+                     
+                        <a  id="idms-slide-left" class="left carousel-control" href="#left" role="button" data-slide="prev">
+                            <img class="chevron chevron-right img-responsive" src="images/carousel/arrows/idms-carousel-chevron-left.png" />
+  
+                        <a  id="idms-slide-right"  class="right carousel-control " href="#right" role="button" data-slide="next">
+                                <img class="chevron chevron-right img-responsive" src="images/carousel/arrows/idms-carousel-chevron-right.png" />
+                        </a>
+                    </div> <!-- Carousel -->
+                </div>
+                <div class="row">                       
+                    <span class="see-how-itwork-onsite">
+                     <center>SEE HOW THIS WIDGET WORK WITH THE CONTAINERS SITE</center>
+                    </span>
+                </div>
+                <div class="row">
+                    <center><a href="#"><img src="images/arrow-more-over.png"  alt=""></img></a></center>
+                </div>
+            </div>
+        </div>
+        <div class="row home_about_idata">
+            <div class="container">
+                <div class="row"  style="padding-top:60px">
+                           <span class="home_about_idata-title">ABOUT INTERACTIVE DATA </span> 
+                        </div>
+                        <div class="row home_about_idata-body" >
+                             <p style="padding-left:20px">
+                                venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. 
+                                Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. 
+                                Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, 
+                                dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. 
+                                Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. 
+                                Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero,
+                                sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. 
+                                Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. 
+                                Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla 
+                                mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales,
+                                augue velit cursus nunc,  
+                             </p>
+                            <p style="padding-left:20px">
+                                venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. 
+                                Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. 
+                                Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, 
+                                dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. 
+                                Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. 
+                                Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero,
+                                sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. 
+                                Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. 
+                                Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla 
+                                mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales,
+                                augue velit cursus nunc,  
+                             </p>
+                        </div>
+                        <div class="row">
+                    <center><a href="#"><img src="images/back.png"  alt=""></img></a></center>
+                </div>
+            </div>
+        </div>
+<?php require_once 'footer.php'; ?>
+
